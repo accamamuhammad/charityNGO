4e405ca1a87a50c20abdd44d6eba22887c2b2566
accamamuhammad@gmail.com <accamamuhammad@gmail.com>
2024-10-27 16:17:00 +0100

    build fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import HeroPage from "./components/HeroPage";
 import Testimonial from "./components/testimonial";
 import AboutUs from "./components/AboutUs";
