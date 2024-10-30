94e32715296c09f0da360fb3fce343f658455921
accamamuhammad@gmail.com <accamamuhammad@gmail.com>
2024-10-30 21:15:16 +0100

    build error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroPage from "./components/HeroPage";
-import Testimonial from "./components/testimonial";
+import Testimonial from "./components/Testimonial";
 import AboutUs from "./components/AboutUs";
 import Services from "./components/Services";
 import Events from "./components/Events";
