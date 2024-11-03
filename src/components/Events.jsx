d1674e2f53c5bdcddb8fa9dc96efce9344590d01
accama <accamamuhammad@gmail.com>
2024-11-03 08:50:09 +0100

    events fix

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import EventBox from "./common/EventBox";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import eventImage1 from "../assets/eventsImages/l1.jpg";
 import eventImage2 from "../assets/eventsImages/l2.jpg";
 import eventImage3 from "../assets/eventsImages/l3.jpg";
@@ -7,19 +9,21 @@ import eventImage4 from "../assets/eventsImages/l4.jpg";
 
 const Events = () => {
   return (
-    <div className="w-full bg-mainBg px-8 py-7 space-y-6">
-      <div className="space-y-2.5">
-        {" "}
-        <h1 className="text-2xl text-white">
-          Our{" "}
-          <span className="underline underline-offset-4 decoration-yellow-300 decoration-4">
-            Exclusive events
-          </span>
-        </h1>
-        <p className="text-white opacity-75 text-sm leading-6">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
-          praesentium ipsam.
-        </p>
+    <div className="w-full bg-mainBg px-8 py-7 space-y-6 relative">
+      <div className="flex flex-row gap-3 justify-between items-end">
+        <div className="space-y-2.5">
+          <h1 className="text-2xl text-white">
+            Our{" "}
+            <span className="underline underline-offset-4 decoration-yellow-300 decoration-4">
+              Exclusive events
+            </span>
+          </h1>
+          <p className="text-white opacity-75 text-sm leading-6">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            praesentium ipsam.
+          </p>
+        </div>
+        <FontAwesomeIcon icon={faArrowRight} color="yellow" />
       </div>
       <div className="flex overflow-x-auto last:overflow-y-hidden whitespace-nowrap gap-5 py-2">
         <div className="inline-flex space-x-8">
--- src/components/common/EventBox.jsx
+++ src/components/common/EventBox.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const EventBox = (props) => {
   return (
-    <div className="w-80 h-[355px] rounded-lg flex flex-col gap-2.5 items-start justify-start text-white">
-      <img src={props.image} alt="" className="w-full h-[175px] rounded-md" />
-      <h1 className="mt-1 font-medium text-xl">{props.title}</h1>
-      <h3 className="text-sm font-medium opacity-70">12 years old</h3>
+    <div className="w-[300px] h-[400px] rounded-lg flex flex-col gap-3.5 items-start justify-start text-white">
+      <img src={props.image} alt="" className="w-full h-[175px] rounded-lg" />
+      <h1 className="mt-1 font-medium text-2xl">{props.title}</h1>
+      <h3 className="font-medium opacity-70">12 years old</h3>
       <p className="text-sm opacity-75 leading-6 text-wrap">
         {props.description}
       </p>
